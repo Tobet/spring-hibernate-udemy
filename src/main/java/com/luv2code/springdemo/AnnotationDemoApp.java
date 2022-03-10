6e2bb87e3bbc14abbce7da3ef007bc4b716f2e87
tobet <tommasobetti91@gmail.com>
2022-03-10 23:37:27 +0100

    autowiring

--- src/main/java/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/main/java/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -12,8 +12,11 @@ public class AnnotationDemoApp {
         // get the bean from spring container
         Coach coach = context.getBean("tennisCoach", Coach.class);
 
-        // call a method on the bean
+        //   call a method on the bean
         System.out.println(coach.getDailyWorkout());
+
+        // call method to get the daily fortune
+        System.out.println(coach.getDailyFortune());
 
         // close the context
         context.close();
--- src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,52 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component // -> this is the bean id | if we don't specify bean id, class name with lower case first letter will be taken by spring
+@Component
+// -> this is the bean id | if we don't specify bean id, class name with lower case first letter will be taken by spring
 public class TennisCoach implements Coach {
+
+    @Autowired
+    private FortuneService fortuneService;
+
+    public TennisCoach() {
+        System.out.println(">> inside default constructor");
+    }
+
+    /*
+    // constructor injection
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+    */
+
+    /*
+    // define a setter method - setter injection
+    @Autowired
+    public void setFortuneService(FortuneService fService) {
+        System.out.println(">> inside setFortuneService method");
+        fortuneService = fService;
+    }
+    */
+
+    /*
+    // method injection
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fService) {
+        System.out.println(">> inside doSomeCrazyStuff method");
+        fortuneService = fService;
+    }
+    */
 
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
     }
 
-
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
 }
