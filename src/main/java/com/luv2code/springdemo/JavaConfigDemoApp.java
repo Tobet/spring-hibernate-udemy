9494b37739ebbd94a59bbcd3d44c661061de1293
tobet <tommasobetti91@gmail.com>
2022-04-10 20:25:47 +0200

    java configuration of spring context

---
+++ src/main/java/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // get the bean from spring container
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+
+        //   call a method on the bean
+        System.out.println(coach.getDailyWorkout());
+
+        // call method to get the daily fortune
+        System.out.println(coach.getDailyFortune());
+
+        // close the context
+        context.close();
+    }
+}
