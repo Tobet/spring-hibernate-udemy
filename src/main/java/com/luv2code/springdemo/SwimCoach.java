24659d3f8b744b50e3a321e222f31ab5f35ac14b
tobet <tommasobetti91@gmail.com>
2022-04-11 23:27:24 +0200

    get properties from properties file in java config file

--- src/main/java/com/luv2code/springdemo/SwimCoach.java
+++ src/main/java/com/luv2code/springdemo/SwimCoach.java
@@ -1,11 +1,27 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
     private final FortuneService fortuneService;
 
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
     public SwimCoach(FortuneService theFortuneService) {
         this.fortuneService = theFortuneService;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
     @Override
