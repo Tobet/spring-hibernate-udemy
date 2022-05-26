d9af89436bfab8a0c2b3cf42c33c793458d13538
Tommaso Betti <tommaso.betti@nttdata.com>
2022-05-26 15:44:50 +0200

    generated a spring boot project becasue I wasn't able to startup a spring mvc legacy project (old project is in spring-mvc branch)

---
+++ src/main/java/com/tobet/springmvc/GreetingController.java
@@ -0,0 +1,13 @@
+package com.tobet.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "greeting";
+    }
+}
